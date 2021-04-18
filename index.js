d2c67a1e8c4da3be7f972fcaeef4fb118737909f
William Elderkin <william_elderkin@protonmail.com>
2021-04-18 15:36:05 -0400

    playRound function added and debugged

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 function computerPlay() {
-    const handGestures = ["Rock", "Paper", "Scissors"];
+    const handGestures = ["rock", "paper", "scissors"];
     const computerChoice = handGestures[Math.floor(Math.random() * handGestures.length)];
-    console.log(computerChoice);
+    return(computerChoice);
 }
 
-computerPlay()
+
+
+function playRound(playerSelection, computerSelection) {
+    
+  
+
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        return(alert("You Lose! Paper covers Rock."));
+    }
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        return(alert("You Win! Rock smashes Scissors."));
+    }
+    if (playerSelection == "paper" && computerSelection == "rock") {
+        return(alert("You Win! Paper covers Rock."));
+    }
+    if (playerSelection == "paper" && computerSelection == "scissors") {
+        return(alert("You Lose! Scissors cut Paper."));
+    }
+    if (playerSelection == "scissors" && computerSelection == "paper") {
+        return(alert("You Win! Scissors cut Paper."));
+    }
+    if (playerSelection == "scissors" && computerSelection == "rock") {
+        return(alert("You Lose! Rock smashes Scissors."));
+    }
+    else return(alert("It's a Tie!"))
+
+}
+
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
