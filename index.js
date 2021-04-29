ffc76a6032ab8a1593ad1e6e276b47ccaf1aba76
William Elderkin <william_elderkin@protonmail.com>
2021-04-29 16:10:37 -0400

    fully functioning game function, added finalScore function

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 function computerPlay() {
     const handGestures = ["rock", "paper", "scissors"];
-    const computerChoice = handGestures[Math.floor(Math.random() * handGestures.length)];
+    let computerChoice = handGestures[Math.floor(Math.random() * handGestures.length)];
     return(computerChoice);
 }
 
+let playerPoints = 0;
+let computerPoints = 0;
 
 
 function playRound(playerSelection, computerSelection) {
-    
-    let playerPoints = 0;
-    let computerPoints = 0;
-     
-
     if (playerSelection == "rock" && computerSelection == "paper") {
         computerPoints += 1;
         return(`You Lose! Paper covers Rock. Player Points = ${playerPoints} Computer Points = ${computerPoints}`)
@@ -37,16 +34,29 @@ function playRound(playerSelection, computerSelection) {
         return(`You Lose! Rock smashes Scissors.Player Points = ${playerPoints} Computer Points = ${computerPoints}`)
     }
     else return("It's a Tie!")
-
 }
+
 function game() {
-    const playRound = playRound(playerSelection, computerSelection)
-    while (playerPoints || computerPoints > 5) {
-    playRound(playerSelection, computerSelection);}
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, Paper, or Scissors?");
+        let computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    }
 }
 
-const playerSelection = prompt("Rock, Paper, or Scissors?");
-const computerSelection = computerPlay();
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+function finalScore(playerPoints, computerPoints) {
+   
+    if (playerPoints > computerPoints) {
+        console.log(`You beat the computer! ${playerPoints} to ${computerPoints}`);
+    }
+    if (playerPoints < computerPoints) {
+        console.log(`The computer beat you! ${computerPoints} to ${playerPoints}`);
+    }
+    if (playerPoints == computerPoints) {
+        console.log(`Final score is a tie! ${playerPoints} to ${computerPoints}`);
+    }
+}
+
+game();
+finalScore(playerPoints, computerPoints);
+
